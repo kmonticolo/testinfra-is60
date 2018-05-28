771d3701b9bbe9f9820276a11e3b66c1d779daba
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-05-28 22:20:57 +0200

    checks for ntp.conf server

--- test_IS60_INTEG_APP1.py
+++ test_IS60_INTEG_APP1.py
@@ -5,6 +5,13 @@ def test_uname_output(Command):
     assert command.stdout.rstrip() == 'Linux'
     assert command.rc == 0
 
+def test_ntp_conf(File):
+    ntp_conf= File("/etc/ntp.conf")
+    assert ntp_conf.user == "root"
+    assert ntp_conf.group == "root"
+    assert ntp_conf.mode == 0o644
+    assert ntp_conf.contains("server 128.168.160.95")
+
 def test_NTP_ntpstat(Command):
     command = Command('ntpstat')
     assert command.rc == 0
