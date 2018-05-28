a29fc85a1f97c1d161dc2db349ad468c77859319
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-05-29 00:40:21 +0200

    typo

--- testall.sh
+++ testall.sh
@@ -9,8 +9,8 @@ py.test test_IS60_INTEG_APP_common.py test_IS60_INTEG_APP5_sockets.py --ssh-conf
 py.test test_IS60_INTEG_APP_common.py test_IS60_INTEG_APP6_sockets.py --ssh-config=/tmp/sshconfig --hosts 128.168.160.198  -v -n4
 
 #databases with drbd
-py.test test_IS60$INTEG_DB_common.py test_IS60_INTEG_DB_drbd.py test_IS60_INTEG_DB1_sockets.py --ssh-config=/tmp/sshconfig --hosts 128.168.160.190 -v -n4
-py.test test_IS60$INTEG_DB_common.py test_IS60_INTEG_DB_drbd.py test_IS60_INTEG_DB2_sockets.py --ssh-config=/tmp/sshconfig --hosts 128.168.160.191 -v -n4
+py.test test_IS60_INTEG_DB_common.py test_IS60_INTEG_DB_drbd.py test_IS60_INTEG_DB1_sockets.py --ssh-config=/tmp/sshconfig --hosts 128.168.160.190 -v -n4
+py.test test_IS60_INTEG_DB_common.py test_IS60_INTEG_DB_drbd.py test_IS60_INTEG_DB2_sockets.py --ssh-config=/tmp/sshconfig --hosts 128.168.160.191 -v -n4
 
 # without drbd
   py.test test_IS60_INTEG_DB_common.py test_IS60_INTEG_DB3_sockets.py --ssh-config=/tmp/sshconfig --hosts 128.168.160.192  -v -n4
