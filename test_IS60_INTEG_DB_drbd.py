f8f4267f681df734baa6d096a644158782c3f038
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-06-11 15:16:03 +0200

    add crm_verify checks

--- test_IS60_INTEG_DB_drbd.py
+++ test_IS60_INTEG_DB_drbd.py
@@ -27,3 +27,12 @@ def drbd_zabbix_conf(File):
     assert drbd.group == "root"
     assert drbd.mode == 0o644
     assert drbd.contains("drbdadm")
+
+def drbd_crm_verify_xml(Command):
+    command = Command('crm_verify -x /var/lib/heartbeat/crm/cib.xml')
+    assert command.rc == 0
+
+def drbd_crm_verify_consistency_in_running_cluster(Command):
+    command = Command('crm_verify -L')
+    assert command.rc == 0
+
