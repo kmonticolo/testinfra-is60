2d67838dc6b46d843d09577ef0513c9723ba2008
Kamil Monticolo <kmonticolo@dcclabs.tv>
2018-06-13 08:37:12 +0200

    add zabbix db tests

---
+++ test_IS60_INTEG_DB_zabbix.py
@@ -0,0 +1,40 @@
+
+def test_diskstats_zabbix_conf(File):
+    file= File("/etc/zabbix/zabbix_agentd.d/userparameter_diskstats.conf")
+    assert file.user == "root"
+    assert file.group == "root"
+    assert file.mode == 0o644
+
+
+def test_drbd_zabbix_conf(File):
+    file= File("/etc/zabbix/zabbix_agentd.d/userparameter_drbd.conf")
+    assert file.user == "root"
+    assert file.group == "root"
+    assert file.mode == 0o644
+
+
+def test_pacemaker_zabbix_conf(File):
+    file= File("/etc/zabbix/zabbix_agentd.d/zabbix-agent-pacemaker.conf")
+    assert file.user == "root"
+    assert file.group == "root"
+    assert file.mode == 0o644
+
+
+def test_pacemaker_actions(File):
+    file= File("/usr/local/bin/check_pacemaker_actions")
+    assert file.user == "root"
+    assert file.group == "root"
+
+
+def test_crm_mon_stats(File):
+    file= File("/usr/local/bin/crm_mon_stats.sh")
+    assert file.user == "root"
+    assert file.group == "root"
+
+
+
+#/etc/sudoers wpis zabbix  ALL=(ALL)       NOPASSWD: /sbin/drbdadm
+
+
+ #odpowiednie wpisy w sudoers
+
